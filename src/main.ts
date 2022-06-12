c61c302c1707f610a15c5b2d487609ce4929d124
Alexey Valko <valko.alexey@gmail.com>
2022-06-12 18:02:17 +0300

    refactoreL rename swagger

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { handleExceptions } from './utils/handleExceptions';
-import { buildSwaggerDocs } from './utils/swagger';
+import { buildSwaggerDocs } from './utils/buildSwaggerDocs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const { port, host } = configService.get('server');
 
-  buildSwaggerDocs(app);
+  buildSwaggerDocs(app, 'docs');
 
   await app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
@@ -17,5 +17,4 @@ async function bootstrap() {
 }
 
 bootstrap();
-
 handleExceptions();
