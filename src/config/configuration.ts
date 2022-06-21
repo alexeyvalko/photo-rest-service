e00bc68f6db006e8b2588d04a586c6242c78132f
Alexey Valko <valko.alexey@gmail.com>
2022-06-21 17:25:46 +0300

    fix: fix config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,7 @@
-const isDev = process.env.NODE_ENV === 'development';
-const host = isDev ? process.env.LOCAL_HOST : process.env.DEPLOY_HOST;
 export default () => ({
   server: {
     port: parseInt(process.env.PORT, 10) || 5000,
-    host: host || '0.0.0.0',
+    host: process.env.HOST || '0.0.0.0',
   },
   unsplash: {
     accessKey: process.env.UNSPLASH_ACCESS_KEY,
