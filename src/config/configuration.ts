bfe787e41c76e38523cce2d71c5a1c6f46f881fc
Alexey Valko <valko.alexey@gmail.com>
2022-06-21 17:12:50 +0300

    feat: add crosenv

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,9 @@
+const isDev = process.env.NODE_ENV === 'development';
+const localhost = isDev || process.env.HOST;
 export default () => ({
   server: {
     port: parseInt(process.env.PORT, 10) || 5000,
-    host: process.env.HOST || '0.0.0.0',
+    host: localhost || '0.0.0.0',
   },
   unsplash: {
     accessKey: process.env.UNSPLASH_ACCESS_KEY,
